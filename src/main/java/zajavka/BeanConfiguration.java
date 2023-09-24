c2ff64b734a7794756eeb90e23226fd87f244c18
Mikołaj Król <109384291+krlmikolaj@users.noreply.github.com>
2023-09-24 15:37:21 +0200

    Add files via upload

---
+++ src/main/java/zajavka/BeanConfiguration.java
@@ -0,0 +1,11 @@
+package zajavka;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackageClasses = Marker.class)
+public class BeanConfiguration {
+
+
+}
---
+++ src/main/java/zajavka/WidthCalculationService.java
@@ -0,0 +1,7 @@
+package zajavka;
+
+import java.math.BigDecimal;
+
+public interface WidthCalculationService {
+    BigDecimal calculate(InputData inputData);
+}
